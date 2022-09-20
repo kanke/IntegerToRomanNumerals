a47b42c69a5403f82c0952cba89fe7eb8b8d3d17
Kanke <sugarkanke@yahoo.com>
2022-09-20 09:33:36 +0100

    Adding second passing test case using zero value. Ammending gitignore

--- src/main/java/org/ford/NumberToRomanConverter.java
+++ src/main/java/org/ford/NumberToRomanConverter.java
@@ -2,6 +2,11 @@ package org.ford;
 
 public class NumberToRomanConverter {
     public String convertToNumerals(Integer input) {
+        if (input != null && input == 0) {
+            throw new IllegalArgumentException(
+                    String.format("The number must be greater than 0 for Roman Numerals")
+            );
+        }
         return null;
     }
 }
--- src/test/java/org/ford/NumberToNumeralsConverterTest.java
+++ src/test/java/org/ford/NumberToNumeralsConverterTest.java
@@ -1,21 +1,29 @@
 package org.ford;
 
-import org.ford.NumberToRomanConverter;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NumberToNumeralsConverterTest {
-    @ParameterizedTest
+
     @DisplayName("Should pass a null message to our test method")
+    @ParameterizedTest(name = "#{index} - Test with Integer : {0}")
     @NullSource
     void shouldReturnNullForNullInputs(Integer input) {
         NumberToRomanConverter numberConverter = new NumberToRomanConverter();
-
         assertEquals(input, numberConverter.convertToNumerals(input));
+    }
 
+    @DisplayName("Should pass a zero number to our test method")
+    @ParameterizedTest(name = "#{index} - Test with Int : {0}")
+    @ValueSource(ints = {0})
+    void shouldReturnErrorForZeroNumber(int number) {
+        NumberToRomanConverter numberConverter = new NumberToRomanConverter();
+        assertThrows(IllegalArgumentException.class,
+                ()-> assertEquals(number, numberConverter.convertToNumerals(number)));
     }
 }
