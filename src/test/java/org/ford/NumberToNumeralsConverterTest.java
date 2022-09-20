e1b580718469f4eb77c340e33077679683b70e68
Kanke <sugarkanke@yahoo.com>
2022-09-20 10:43:52 +0100

    Adding seventh failing test case using number above 3000.

--- src/test/java/org/ford/NumberToNumeralsConverterTest.java
+++ src/test/java/org/ford/NumberToNumeralsConverterTest.java
@@ -89,4 +89,14 @@ public class NumberToNumeralsConverterTest {
         NumberToRomanConverter numberConverter = new NumberToRomanConverter();
         assertEquals(romanNumeral, numberConverter.convertToNumerals(number));
     }
+
+    @DisplayName("Should pass a non zero number from above 3000 to our test method")
+    @ParameterizedTest(name = "#{index} - Test with Int : {0}")
+    @CsvSource({
+            "4000, IV"
+    })
+    void shouldNotReturnRomanNumeralForNumberAbove3000(int number, String romanNumeral) {
+        NumberToRomanConverter numberConverter = new NumberToRomanConverter();
+        assertEquals(romanNumeral, numberConverter.convertToNumerals(number));
+    }
 }
